3b0b45ef3d100b1dd4e14962379869d5f6a89f58
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-03-19 04:35:00 +0700

    by call api instead of filesystem

--- src/routes/api/filsuf/+server.ts
+++ src/routes/api/filsuf/+server.ts
@@ -1,17 +1,18 @@
 import { json, type RequestHandler } from '@sveltejs/kit'
-import csvParser from 'csv-parser'
-import fs from 'fs'
-import path from 'path'
+import { parse } from 'csv-string'
 import type { KataKataInspirasi } from '../../../../../../../../Workspace/BINSAR/program/jagoankata/src/app'
 
-export const GET: RequestHandler = async ({ setHeaders}) => {
-    const data: KataKataInspirasi[] = []
-    await new Promise((resolve) => {
-        const filepath = path.join('../../..', 'data', 'filsuf.csv')
-        fs.createReadStream(filepath)
-            .pipe(csvParser())
-            .on('data', d => data.push(d))
-            .on('end', resolve)
+export const GET: RequestHandler = async ({ setHeaders,url}) => {
+    let data: KataKataInspirasi[] = []
+    await new Promise(async (resolve) => {
+        const target=  new URL(url.toString())
+        target.pathname='/data/filsuf.csv'
+        const resp = await fetch(target.toString())
+        // @ts-ignore
+        data=(parse(await resp.text(), {
+            output:"objects"
+        }))
+        resolve(true)
     })
     setHeaders({
         'cache-control': 'public,max-age: 60'
