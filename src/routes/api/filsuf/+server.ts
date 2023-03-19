93de27d999d2be8d933f3e19396d195ec85c4786
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2023-03-19 16:33:57 +0700

    update sourcde

--- src/routes/api/filsuf/+server.ts
+++ src/routes/api/filsuf/+server.ts
@@ -2,18 +2,31 @@ import { json, type RequestHandler } from '@sveltejs/kit'
 import { parse } from 'csv-string'
 import type { KataKataInspirasi } from '../../../../../../../../Workspace/BINSAR/program/jagoankata/src/app'
 
-export const GET: RequestHandler = async ({ setHeaders,url}) => {
+export const GET: RequestHandler = async ({ setHeaders, url }) => {
     let data: KataKataInspirasi[] = []
-    await new Promise(async (resolve) => {
-        const target=  new URL(url.toString())
-        target.pathname='/data/filsuf.csv'
+
+    const urls = [
+        'https://raw.githubusercontent.com/binsarjr/koleksi-jagoankata/results/filsuf.csv',
+        'https://github.com/binsarjr/koleksi-jagoankata/raw/results/brainyquote.csv',
+        'https://github.com/binsarjr/koleksi-jagoankata/raw/results/www.goodreads.com.csv'
+    ]
+    const target = new URL(url.toString())
+    target.pathname = '/data/filsuf.csv'
+    urls.push(target.toString())
+
+    const results = await Promise.all(urls.map(async url => {
         const resp = await fetch(target.toString())
         // @ts-ignore
-        data=(parse(await resp.text(), {
-            output:"objects"
-        }))
-        resolve(true)
-    })
+        return parse(await resp.text(), {
+            output: "objects"
+        }) as KataKataInspirasi[]
+    }))
+
+
+    data = results.flat()
+
+
+
     setHeaders({
         'cache-control': 'public,max-age: 60'
     })
